d48995bc2a0a21ede40ba1581ad14bce08f001fa
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-06-29 13:43:20 +0530

    added

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Student {
 
     @Id
@@ -16,56 +18,5 @@ public class Student {
     private String mobile;
     private String address;
 
-    // Constructors, getters, setters, and toString method
-    public Student() {
-    }
-
-    public Student(String name, String mobile, String address) {
-        this.name = name;
-        this.mobile = mobile;
-        this.address = address;
-    }
-
-    // Getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", mobile='" + mobile + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
+   
 }
